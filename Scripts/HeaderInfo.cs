3264a89ffe891c7bd02bc13acb3af1d09e5152d1
JonGrayson <49501559+JonGrayson@users.noreply.github.com>
2020-10-14 22:24:41 -0700

    Updates

---
+++ Scripts/HeaderInfo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class HeaderInfo : MonoBehaviourPun
+{
+    public TextMeshProUGUI nameText;
+    public Image bar;
+    private float maxValue;
+
+    public void Initialize(string nickName, int maxVal)
+    {
+        nameText.text = nickName;
+        maxValue = maxVal;
+        bar.fillAmount = 1.0f;
+    }
+
+    [PunRPC]
+    void UpdateHealthBar(int value)
+    {
+        bar.fillAmount = value / maxValue;
+    }
+}
